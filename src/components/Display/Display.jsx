ad757bf2bb765e0b14e4e1de7818b2fb490e2333
Olamide Atitebi <olamideumarq@gmail.com>
2022-09-06 07:24:35 +0100

    fix: fix form a11y and HTML issues

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,85 +1,83 @@
-import React, { useEffect, useRef } from "react";
-import { useContext } from "react";
-import styles from "./Display.module.css";
-import InputContext from "../../context/InputContext";
+import React, { useEffect, useRef } from 'react';
+import { useContext } from 'react';
+import styles from './Display.module.css';
+import InputContext from '../../context/InputContext';
 function Input() {
-    const { inputState, dispatch, ACTIONS } = useContext(InputContext);
-    const inputRef = useRef();
-    // when the Display state changes, let the input element be in focus and
-    // set the caret position to the current value in the display state
-    useEffect(() => {
-        inputRef.current.focus();
-        inputRef.current.setSelectionRange(
-            inputState.selectionStart,
-            inputState.selectionEnd
-        );
-    }, [inputState]);
-
-    // enable state change through keyboard events
-    const handleChange = (event) => {
-        switch (event.nativeEvent.inputType) {
-            case "deleteContentBackward":
-                dispatch({ type: ACTIONS.DELETE_BACKWARD });
-                break;
-            case "deleteContentForward":
-                dispatch({ type: ACTIONS.DELETE_FORWARD });
-                break;
-            case "deleteByCut":
-                dispatch({ type: ACTIONS.DELETE_SELECTION });
-                break;
-            case "insertFromPaste":
-                navigator.clipboard.readText().then((text) => {
-                    dispatch({
-                        type: ACTIONS.DISPLAY,
-                        payload: { data: text },
-                    });
-                });
-                break;
-            default:
-                dispatch({
-                    type: ACTIONS.DISPLAY,
-                    payload: { data: event.nativeEvent.data },
-                });
-                break;
-        }
-    };
-
-    // update caret position onSelectionChange
-    const handleSelectionChange = () => {
-        dispatch({
-            type: ACTIONS.SELECTION_CHANGE,
-            payload: {
-                selectionStart: inputRef.current.selectionStart,
-                selectionEnd: inputRef.current.selectionEnd,
-            },
-        });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        dispatch({ type: ACTIONS.EVALUATE });
-    };
-    return (
-        <form
-            className={styles.display}
-            onSubmit={handleSubmit}
-            id="display-form"
-        >
-            <input
-                onInput={handleChange}
-                onSelect={handleSelectionChange}
-                className={styles.input}
-                type="text"
-                value={inputState.expression}
-                ref={inputRef}
-            />
-            <input
-                type="text"
-                value={inputState.evaluation}
-                className={`${styles.input} ${styles["input-display"]}`}
-                readOnly
-            />
-        </form>
+  const { inputState, dispatch, ACTIONS } = useContext(InputContext);
+  const inputRef = useRef();
+  // when the Display state changes, let the input element be in focus and
+  // set the caret position to the current value in the display state
+  useEffect(() => {
+    inputRef.current.focus();
+    inputRef.current.setSelectionRange(
+      inputState.selectionStart,
+      inputState.selectionEnd
     );
+  }, [inputState]);
+
+  // enable state change through keyboard events
+  const handleChange = (event) => {
+    switch (event.nativeEvent.inputType) {
+      case 'deleteContentBackward':
+        dispatch({ type: ACTIONS.DELETE_BACKWARD });
+        break;
+      case 'deleteContentForward':
+        dispatch({ type: ACTIONS.DELETE_FORWARD });
+        break;
+      case 'deleteByCut':
+        dispatch({ type: ACTIONS.DELETE_SELECTION });
+        break;
+      case 'insertFromPaste':
+        navigator.clipboard.readText().then((text) => {
+          dispatch({
+            type: ACTIONS.DISPLAY,
+            payload: { data: text },
+          });
+        });
+        break;
+      default:
+        dispatch({
+          type: ACTIONS.DISPLAY,
+          payload: { data: event.nativeEvent.data },
+        });
+        break;
+    }
+  };
+
+  // update caret position onSelectionChange
+  const handleSelectionChange = () => {
+    dispatch({
+      type: ACTIONS.SELECTION_CHANGE,
+      payload: {
+        selectionStart: inputRef.current.selectionStart,
+        selectionEnd: inputRef.current.selectionEnd,
+      },
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch({ type: ACTIONS.EVALUATE });
+  };
+  return (
+    <form className={styles.display} onSubmit={handleSubmit} id="display-form">
+      <input
+        onInput={handleChange}
+        onSelect={handleSelectionChange}
+        className={styles.input}
+        type="text"
+        value={inputState.expression}
+        ref={inputRef}
+        aria-label="input"
+      />
+      <input
+        type="text"
+        value={inputState.evaluation}
+        className={`${styles.input} ${styles['input-display']}`}
+        readOnly
+        aria-label="display"
+      />
+    </form>
+  );
 }
 export default Input;
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,47 +1,46 @@
-import React, { useContext, useEffect } from "react";
-import PropTypes from "prop-types";
-import styles from "./Button.module.css";
-import InputContext from "../../context/InputContext";
+import React, { useContext, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import styles from './Button.module.css';
+import InputContext from '../../context/InputContext';
 
 function Button({ children, data, type, version, form }) {
-    const { dispatch, ACTIONS } = useContext(InputContext);
+  const { dispatch, ACTIONS } = useContext(InputContext);
 
-    const handleClick = () => {
-        switch (version) {
-            case "display":
-                dispatch({ type: ACTIONS.DISPLAY, payload: { data } });
-                break;
-            case "reset":
-                dispatch({ type: ACTIONS.RESET });
-                break;
-            case "delete":
-                dispatch({ type: ACTIONS.DELETE_BACKWARD });
-            default:
-                break;
-        }
-    };
-    return (
-        <button
-            type={type}
-            onClick={handleClick}
-            className={`${styles.btn} ${styles[`btn-${version}`]}`}
-            form={form}
-        >
-            {children}
-        </button>
-    );
+  const handleClick = () => {
+    switch (version) {
+      case 'display':
+        dispatch({ type: ACTIONS.DISPLAY, payload: { data } });
+        break;
+      case 'reset':
+        dispatch({ type: ACTIONS.RESET });
+        break;
+      case 'delete':
+        dispatch({ type: ACTIONS.DELETE_BACKWARD });
+      default:
+        break;
+    }
+  };
+  const attributes = {
+    className: `${styles['btn']} ${styles[`btn-${version}`]}`,
+    type,
+    onClick: handleClick,
+  };
+  if (form) {
+    attributes.form = form;
+  }
+  return <button {...attributes}>{children}</button>;
 }
 
 Button.defaultProps = {
-    type: "button",
-    version: "display",
-    data: "",
-    form: "",
+  type: 'button',
+  version: 'display',
+  data: '',
+  form: '',
 };
 
 Button.propTypes = {
-    type: PropTypes.string,
-    children: PropTypes.node,
-    display: PropTypes.string,
+  type: PropTypes.string,
+  children: PropTypes.node,
+  display: PropTypes.string,
 };
 export default Button;
